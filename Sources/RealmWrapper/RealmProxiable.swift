6560208e44c89aeb6af6836bafd99cf949eaee97
SH-OH <dhtkdgh429@gmail.com>
2020-04-09 12:58:15 +0900

    Fix Force Unwrapping

--- Sources/RealmWrapper/RealmProxiable.swift
+++ Sources/RealmWrapper/RealmProxiable.swift
@@ -12,7 +12,9 @@ public extension RealmProxiable {
     }
     
     func query<T: Object>(_ type: T.Type = T.self, filter: String? = nil, sortProperty: String? = nil, ordering: OrderingType = .ascending) -> RealmQuery<T> {
-        let realm = try! Realm(configuration: rm.createConfiguration())
+        guard let realm = try? Realm(configuration: rm.createConfiguration()) else {
+            fatalError("RealmProxiable not find to database")
+        }
         var results = realm.objects(type)
         if let filter = filter {
             results = results.filter(filter)
