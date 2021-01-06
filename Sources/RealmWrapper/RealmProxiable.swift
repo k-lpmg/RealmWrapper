e679945805e0512d3cf6fa6de1dd55e3cbd7f2c9
k-lpmg <kanglpmg@gmail.com>
2021-01-06 13:13:37 +0900

    Add query using NSPredicate in RealmProxiable

--- Sources/RealmWrapper/RealmProxiable.swift
+++ Sources/RealmWrapper/RealmProxiable.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 import RealmSwift
 
 public protocol RealmProxiable {
@@ -31,5 +33,26 @@ public extension RealmProxiable {
         
         return RealmQuery(results: results)
     }
-    
+
+    func query<T: Object>(
+        _ type: T.Type = T.self,
+        filter: NSPredicate? = nil,
+        sortProperty: String? = nil,
+        ordering: OrderingType = .ascending
+    ) -> RealmQuery<T> {
+        guard let realm = try? Realm(configuration: rm.createConfiguration()) else {
+            return RealmQuery(results: nil)
+        }
+
+        var results = realm.objects(type)
+        if let filter = filter {
+            results = results.filter(filter)
+        }
+        if let sortProperty = sortProperty {
+            results = results.sorted(byKeyPath: sortProperty, ascending: ordering == .ascending)
+        }
+
+        return RealmQuery(results: results)
+    }
+
 }
